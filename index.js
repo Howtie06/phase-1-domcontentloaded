31b00df31493af4ccbd8a12897f0ab978bb8ce76
audrey cherop <audreycherop06@gmail.com>
2024-04-04 15:24:56 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+   const element = this.getElementById("text");
+   element.innerHTML="This is really cool!"
+  });
+  
